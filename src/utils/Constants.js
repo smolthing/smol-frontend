bbe1ec19fb1bda42bc5b1dbddbd9eba4915eed59
mysmolthing <mysmolthing@gmail.com>
2024-03-03 01:18:23 +0800

    Update error message

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,7 @@
 export const ERROR_MAPPING = {
     'auth/invalid-email': 'Invalid email.',
     'auth/invalid-login-credentials': 'Invalid login credentials.',
-    'auth/weak-password': 'Password should be at least 6 characters',
+    'auth/weak-password': 'Password should be at least 6 characters.',
     nameRequired: 'Name is required.',
     emailRequired: 'Email is required.',
     passwordRequired: 'Password is required.',
